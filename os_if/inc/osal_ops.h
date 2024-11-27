807a94e7737b33dffe942593655a255b9cef0d1f
Ravi Dondaputi <ravi.dondaputi@nordicsemi.no>
2024-11-27 16:41:33 +0530

    Create dedicated memory pools for Wi-Fi
    Create dedicated memory pools for Wi-Fi data and
    management paths. Introduce corresponding memory
    allocation/deallocation calls.
    
    Signed-off-by: Ravi Dondaputi <ravi.dondaputi@nordicsemi.no>

--- os_if/inc/osal_ops.h
+++ os_if/inc/osal_ops.h
@@ -28,13 +28,13 @@ struct nrf_wifi_osal_ops {
 	/**
 	 * @brief Allocate memory.
 	 *
-	 * @param size The size of the memory to allocate.
+	 * @param size The size of the memory to allocate for control messages.
 	 * @return A pointer to the start of the allocated memory.
 	 */
 	void *(*mem_alloc)(size_t size);
 
 	/**
-	 * @brief Allocate zero-initialized memory.
+	 * @brief Allocate zero-initialized memory for control messages.
 	 *
 	 * @param size The size of the memory to allocate.
 	 * @return A pointer to the start of the allocated memory.
@@ -42,11 +42,26 @@ struct nrf_wifi_osal_ops {
 	void *(*mem_zalloc)(size_t size);
 
 	/**
-	 * @brief Free allocated memory.
+	 * @brief Free memory allocated for control messages.
 	 *
 	 * @param buf A pointer to the memory to free.
 	 */
 	void (*mem_free)(void *buf);
+
+	/**
+	 * @brief Allocate zero-initialized memory for data.
+	 *
+	 * @param size The size of the memory to allocate.
+	 * @return A pointer to the start of the allocated memory.
+	 */
+	void *(*data_mem_zalloc)(size_t size);
+
+	/**
+	 * @brief Free memory allocated for data.
+	 *
+	 * @param buf A pointer to the memory to free.
+	 */
+	void (*data_mem_free)(void *buf);
 
 	/**
 	 * @brief Copy memory.
