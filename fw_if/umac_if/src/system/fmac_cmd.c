7cb2f44f46dfc86e4f97477ee90022944e138dd8
Kapil Bhatt <kapil.bhatt@nordicsemi.no>
2025-06-18 06:49:26 +0000

    fw_if: src: system: Set Dynamic ED in init command
    Based on Kconfig option, set Dynamic ED feature
    enabling/disabling in init command.
    
    Signed-off-by: Kapil Bhatt <kapil.bhatt@nordicsemi.no>

--- fw_if/umac_if/src/system/fmac_cmd.c
+++ fw_if/umac_if/src/system/fmac_cmd.c
@@ -174,6 +174,10 @@ enum nrf_wifi_status umac_cmd_sys_init(struct nrf_wifi_fmac_dev_ctx *fmac_dev_ct
 		umac_cmd_data->dbs_war_ctrl = 1;
 	#endif /* NRF_WIFI_DYNAMIC_BANDWIDTH_SIGNALLING */
 
+	#ifdef NRF_WIFI_DYNAMIC_ED
+		umac_cmd_data->dynamic_ed = 1;
+	#endif /* NRF_WIFI_DYNAMIC_ED */
+
 	status = nrf_wifi_hal_ctrl_cmd_send(fmac_dev_ctx->hal_dev_ctx,
 					    umac_cmd,
 					    (sizeof(*umac_cmd) + len));
