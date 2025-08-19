5ba5d0a561f2a396aebac656d5274f7b59237530
Karun Kumar Eagalapati <karun.kumar@nordicsemi.no>
2025-08-19 18:24:23 +0530

    nrf_wifi : Update Wi-Fi FW blobs and shared header file
    Update firmware binaries and RPU shared header to
    control over TWT setup request timeout through kconfig option.
    
    Signed-off-by: Karun Kumar Eagalapati <karun.kumar@nordicsemi.no>

--- fw_if/umac_if/inc/fw/patch_info.h
+++ fw_if/umac_if/inc/fw/patch_info.h
@@ -62,7 +62,7 @@ struct nrf70_fw_image_info {
 #define RPU_FAMILY         (1)
 #define RPU_MAJOR_VERSION   (2)
 #define RPU_MINOR_VERSION   (14)
-#define RPU_PATCH_VERSION   (2)
+#define RPU_PATCH_VERSION   (3)
 
 /**
  * @}
