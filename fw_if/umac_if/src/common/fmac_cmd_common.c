f6d0977b174f30e78965cb534b8ee5c1df03c3df
Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>
2025-10-14 14:10:55 +0530

    Fix build warnings
    Fix the header file inclusions.
    
    Signed-off-by: Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>

--- fw_if/umac_if/src/common/fmac_cmd_common.c
+++ fw_if/umac_if/src/common/fmac_cmd_common.c
@@ -9,12 +9,12 @@
  * FMAC IF Layer of the Wi-Fi driver.
  */
 
+#include "common/hal_api_common.h"
 
 #ifdef NRF71_ON_IPC
 #include <nrf71_wifi_ctrl.h>
 #else
 #include "host_rpu_umac_if.h"
-#include "common/hal_api_common.h"
 #endif
 #include "common/fmac_structs_common.h"
 #include "common/fmac_util.h"
